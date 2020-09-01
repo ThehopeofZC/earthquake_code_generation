45fb96240376f7b7338196a8970777a0765662aa
LightNormal <44899581+LightNormal@users.noreply.github.com>
2020-09-01 18:31:45 +0800

    DialogFormH.py是从头文件产生上传代码，uploadDialog.py从schema目录产生上传代码（因为汉字的原因)

---
+++ DialogFormH.py
@@ -0,0 +1,29 @@
+import os
+
+
+path = input("请输入文件名：")
+outpath=input("请输入文件夹地址，默认为输入目录下:") or path
+infile=open(path,'r')
+File=outpath+'\\'+path[-15:-2]+'upload.txt'
+fc=open(File,"w",encoding='utf-8')
+#F:\MasterStudy\wgtd\地震局活动断裂三维建模公共平台\code\SB_FGDB_API\EsSBData_A2_Geography1_2000.h
+#F:\MasterStudy\wgtd
+strforwrite='bool CSB_GDB_UPLOAD_TOOLDlg::EsUpload'
+for line in infile.readlines():
+    if line[:6]=='struct':
+        tbname= line[8:-1]
+        strforwrite+=tbname+'()\n{\n\tEsGDBReader obReader;\n\t((CEdit*)GetDlgItem(IDC_EDIT_PROCESS_INFO))->ReplaceSel("正在获取'+tbname
+        strforwrite+='...\\r\\n");\n\tvector<_'+tbname+'> vec_'+tbname+';\n\tif(!obReader.EsGet'+tbname+'(vec_'+tbname+'))\n\t{\n\t\t'
+        strforwrite+='((CEdit*)GetDlgItem(IDC_EDIT_PROCESS_INFO))->ReplaceSel("'+tbname+'获取失败！\\r\\n");\n\t\t'
+        strforwrite+='((CEdit*)GetDlgItem(IDC_EDIT_PROCESS_INFO))->ReplaceSel(obReader.EsGetError());\n\t\t'
+        strforwrite+='((CEdit*)GetDlgItem(IDC_EDIT_PROCESS_INFO))->ReplaceSel("\\r\\n");\n\t\treturn false;\n\t}\n\t'
+        strforwrite+='((CEdit*)GetDlgItem(IDC_EDIT_PROCESS_INFO))->ReplaceSel("'+tbname+'获取成功！\\r\\n");\n\t'
+        strforwrite+='((CEdit*)GetDlgItem(IDC_EDIT_PROCESS_INFO))->ReplaceSel("正在上传'
+        strforwrite+=tbname+'...\\r\\n");\n\tif(!EsAdd'+tbname+'ToNetDB(m_nSId, vec_'+tbname+'))\n\t{\n\t\t((CEdit*)GetDlgItem(IDC_EDIT_PROCESS_INFO))->ReplaceSel("'
+        strforwrite+=tbname+'上传失败！");\n\t\t((CEdit*)GetDlgItem(IDC_EDIT_PROCESS_INFO))->ReplaceSel(obReader.EsGetError());\n\t\t'
+        strforwrite+='((CEdit*)GetDlgItem(IDC_EDIT_PROCESS_INFO))->ReplaceSel("\\r\\n");\n\t\treturn false\n\t}\n\t'
+        strforwrite+='((CEdit*)GetDlgItem(IDC_EDIT_PROCESS_INFO))->ReplaceSel("'+tbname+'上传成功！\\r\\n");\n\treturn true;\n}\n'
+
+fc.write(strforwrite)
+fc.close()
+
---
+++ uploadDialog.py
@@ -0,0 +1,39 @@
+import os
+
+path="D:/微信文件/WeChat Files/wxid_0ky67l09jqcy21/FileStorage/File/2020-07/schema(1)/schema"
+#path=input("请输入文件夹地址:")
+
+outpath=input("请输入文件夹地址，默认为输入目录下:") or  path
+filedir=os.listdir(path)
+
+
+for t in filedir:
+    updir=os.path.join(path,t)
+    if os.path.isfile(updir):
+        continue
+    l=os.listdir(updir)
+    File=t+'uploadDialog.txt'
+    fc=open(os.path.join(outpath,File),"w",encoding='utf-8')
+    for index in l:
+        strforwrite='bool CSB_GDB_UPLOAD_TOOLDlg::EsUpload'
+        temp=index.split('-')
+        tbname=''#表英文名
+        if(len(temp[2])>5): #项目表
+            tbname=temp[0][3:]
+        else:
+            tbname=temp[0]
+        strforwrite+=tbname+'()\n{\n\tEsGDBReader obReader;\n\t((CEdit*)GetDlgItem(IDC_EDIT_PROCESS_INFO))->ReplaceSel("正在获取'+temp[1]
+        strforwrite+='...\\r\\n");\n\tvector<_'+tbname.upper()+'> vec_'+tbname+';\n\tif(!obReader.EsGet'+tbname+'(vec_'+tbname+'))\n\t{\n\t\t'
+        strforwrite+='((CEdit*)GetDlgItem(IDC_EDIT_PROCESS_INFO))->ReplaceSel("'+temp[1]+'获取失败！\\r\\n");\n\t\t'
+        strforwrite+='((CEdit*)GetDlgItem(IDC_EDIT_PROCESS_INFO))->ReplaceSel(obReader.EsGetError());\n\t\t'
+        strforwrite+='((CEdit*)GetDlgItem(IDC_EDIT_PROCESS_INFO))->ReplaceSel("\\r\\n");\n\t\treturn false;\n\t}\n\t'
+        strforwrite+='((CEdit*)GetDlgItem(IDC_EDIT_PROCESS_INFO))->ReplaceSel("'+temp[1]+'获取成功！\\r\\n");\n\t'
+        strforwrite+='((CEdit*)GetDlgItem(IDC_EDIT_PROCESS_INFO))->ReplaceSel("正在上传'
+        strforwrite+=temp[1]+'...\\r\\n");\n\tif(!EsAdd'+tbname+'ToNetDB(m_nSId, vec_'+tbname+'))\n\t{\n\t\t((CEdit*)GetDlgItem(IDC_EDIT_PROCESS_INFO))->ReplaceSel("'
+        strforwrite+=temp[1]+'上传失败!");\n\t\t((CEdit*)GetDlgItem(IDC_EDIT_PROCESS_INFO))->ReplaceSel(obReader.EsGetError());\n\t\t'
+        strforwrite+='((CEdit*)GetDlgItem(IDC_EDIT_PROCESS_INFO))->ReplaceSel("\\r\\n");\n\t\treturn false\n\t}\n\t'
+        strforwrite+='((CEdit*)GetDlgItem(IDC_EDIT_PROCESS_INFO))->ReplaceSel("'+temp[1]+'上传成功！\\r\\n");\n\treturn true;\n}\n'
+        fc.write(strforwrite)
+
+    fc.close()
+
